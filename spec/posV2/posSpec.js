f75d6ca9c4b96ee84cb2a9502c5373127f51543b
utlight <joker_e@163.com>
2017-03-27 11:45:32 +0800

    realize the 1 case

--- spec/posV2/posSpec.js
+++ spec/posV2/posSpec.js
@@ -1,22 +1,24 @@
 
 
 describe('Resolve barcode', () => {
-    it('resolve barcode without count', ()=>{
+    let resolveBarcode = require('../../lib/posV2/resolveBarcode')
+
+    it('should resolve barcode without count', ()=>{
         let inputBarcode = ['ITEM000001']
         let expectResult = [{
             barcode:'ITEM000001',
             count:1
         }]
-        expect(resolve(inputBarcode)).toEqual(expectResult)
+        expect(resolveBarcode(inputBarcode)).toEqual(expectResult)
     })
 
-     it('resolve barcode contain count', ()=>{
-        let inputBarcode = ['ITEM000001-3']
-        let expectResult = [{
-            barcode:'ITEM000001',
-            count:3
-        }]
-        expect(resolve(inputBarcode)).toEqual(expectResult)
-    })
+    //  it('should resolve barcode contain count', ()=>{
+    //     let inputBarcode = ['ITEM000001-3']
+    //     let expectResult = [{
+    //         barcode:'ITEM000001',
+    //         count:3
+    //     }]
+    //     expect(resolveBarcode(inputBarcode)).toEqual(expectResult)
+    // })
 });
     
