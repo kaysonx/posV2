121de2c635aa2979a4a9397675248b7136085d15
utlight <joker_e@163.com>
2017-03-27 11:35:56 +0800

    update the test case

--- spec/posV2/posSpec.js
+++ spec/posV2/posSpec.js
@@ -2,20 +2,20 @@
 
 describe('Resolve barcode', () => {
     it('resolve barcode without count', ()=>{
-        let inputBarcode = 'ITEM000001'
-        let expectResult = {
+        let inputBarcode = ['ITEM000001']
+        let expectResult = [{
             barcode:'ITEM000001',
             count:1
-        }
+        }]
         expect(resolve(inputBarcode)).toEqual(expectResult)
     })
 
      it('resolve barcode contain count', ()=>{
-        let inputBarcode = 'ITEM000001'
-        let expectResult = {
-            barcode:'ITEM000001-3',
+        let inputBarcode = ['ITEM000001-3']
+        let expectResult = [{
+            barcode:'ITEM000001',
             count:3
-        }
+        }]
         expect(resolve(inputBarcode)).toEqual(expectResult)
     })
 });
