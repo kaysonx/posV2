49e93e78616ef19e10f14e3c12861c6fb6cd26fc
utlight <joker_e@163.com>
2017-03-27 15:42:44 +0800

    realize count barcodeInfo

--- spec/posV2/posSpec.js
+++ spec/posV2/posSpec.js
@@ -1,24 +1,42 @@
 
 
 describe('Resolve barcode', () => {
-    let resolveBarcode = require('../../lib/posV2/resolveBarcode')
+    let handleBarcode = require('../../lib/posV2/resolveBarcode')
 
-    it('should resolve barcode without count', ()=>{
+    it('should resolve barcode without count', () => {
         let inputBarcode = ['ITEM000001']
         let expectResult = [{
-            barcode:'ITEM000001',
-            count:1
+            barcode: 'ITEM000001',
+            count: 1
         }]
-        expect(resolveBarcode(inputBarcode)).toEqual(expectResult)
+        expect(handleBarcode.resolveBarcode(inputBarcode)).toEqual(expectResult)
     })
 
-     it('should resolve barcode contain count', ()=>{
+    it('should resolve barcode contain count', () => {
         let inputBarcode = ['ITEM000001-3']
         let expectResult = [{
-            barcode:'ITEM000001',
-            count:3
+            barcode: 'ITEM000001',
+            count: 3
         }]
-        expect(resolveBarcode(inputBarcode)).toEqual(expectResult)
+        expect(handleBarcode.resolveBarcode(inputBarcode)).toEqual(expectResult)
     })
+
+
+    it('should count barcode', () => {
+        let inputBarcode = [{
+            barcode: 'ITEM000001',
+            count: 3
+        },
+        {
+            barcode: 'ITEM000001',
+            count: 1
+        }]
+
+        let expectResult = [{
+            barcode:'ITEM000001',
+            count:4
+        }]  
+        expect(handleBarcode.countBarcode(inputBarcode)).toEqual(expectResult)
+    });
+
 });
-    
