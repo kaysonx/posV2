f2ed9c5352045535ab2dc1e4fe41ce6fdb9be834
utlight <joker_e@163.com>
2017-03-27 11:33:16 +0800

    finish the resolve barcode test case

---
+++ spec/posV2/posSpec.js
@@ -0,0 +1,22 @@
+
+
+describe('Resolve barcode', () => {
+    it('resolve barcode without count', ()=>{
+        let inputBarcode = 'ITEM000001'
+        let expectResult = {
+            barcode:'ITEM000001',
+            count:1
+        }
+        expect(resolve(inputBarcode)).toEqual(expectResult)
+    })
+
+     it('resolve barcode contain count', ()=>{
+        let inputBarcode = 'ITEM000001'
+        let expectResult = {
+            barcode:'ITEM000001-3',
+            count:3
+        }
+        expect(resolve(inputBarcode)).toEqual(expectResult)
+    })
+});
+    
